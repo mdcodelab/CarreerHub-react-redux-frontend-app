f64991f0dfc93c0a3612e2409160f07964029333
miha11-hash <d_mihaela@msn.com>
2023-05-02 16:08:40 +0300

    Unauthorized - Logout User

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -44,11 +44,17 @@ export const updateUser = createAsyncThunk('user/updateUser', async (user, thunk
         const resp = await customFetch.patch('/auth/updateUser', user, {
           headers: {
             authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+            //authorization: `Bearer`
           },
         });
         return resp.data;
       } catch (error) {
         console.log(error.response);
+        if(error.response.status === 401) {  //authentication error (happens when we remove the token)
+                thunkAPI.dispatch(logoutUser());
+                toast.warning("Unauthorized! Logging out ...");
+                return thunkAPI.rejectWithValue("Unauthorized! Logging out ...");
+        }
         return thunkAPI.rejectWithValue(error.response.data.msg);
       }
     }
